5a0613d6fb90e2e51b4d820a4c2c2a043170052b
pjestioko <pauljoseph.estioko@gmail.com>
2021-03-03 22:53:11 -1000

    commit recent changes

---
+++ myApp.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+
+const express = require("express ");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+var api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
